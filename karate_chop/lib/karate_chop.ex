ba2f1043013c520adb970a3781e58e64da872418
Philipp Fehre <philipp.fehre@googlemail.com>
2013-06-22 18:05:55 +0200

    make more use of pattern matching...

--- karate_chop/lib/karate_chop.ex
+++ karate_chop/lib/karate_chop.ex
@@ -1,19 +1,21 @@
 defmodule KarateChop do
   import Enum, only: [ at: 2, split: 2, empty?: 1 ]
 
+  defp chop key, [element], offset do
+    cond do
+      key == element -> offset
+      true -> -1
+    end
+  end
+
   defp chop key, list, offset do
     pivot = div(length(list), 2)
     element = at(list, pivot)
     { lower, upper } = split(list, pivot)
     cond do
-      element == key ->
-        pivot + offset
-      length(list) == 1 ->
-        -1
-      element > key ->
-        chop key, lower, offset
-      element < key ->
-        chop key, upper, offset + pivot
+      element == key -> pivot + offset
+      element > key -> chop key, lower, offset
+      element < key -> chop key, upper, offset + pivot
     end
   end
 
